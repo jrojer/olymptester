7540cff33fe332efff51ee7239c59e6efa170976
jrojer <cibid@yandex.ru>
2021-09-20 20:34:20 +0300

    remove Makefile

--- examples/java/run.sh
+++ examples/java/run.sh
@@ -1,1 +1,20 @@
-make && python3 -m olymptester App.class app.txt
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./run.sh [app.cpp | App.java | app.py]"
+fi
+file=$@
+if [[ $file == app.cpp ]]
+then
+    g++ -std=c++11 -o app app.cpp
+    python3 -m olymptester app app.txt 
+fi
+
+if [[ $file == App.java ]]
+then
+    javac App.java
+    python3 -m olymptester App.class app.txt 
+fi
+
+if [[ $file == app.py ]]
+then
+    python3 -m olymptester app.py app.txt 
+fi
