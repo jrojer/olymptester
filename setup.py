f29118c743fb036958881d4f79c03c522f72065d
jrojer <cibid@yandex.ru>
2021-12-18 16:36:12 +0300

    enabale pipx

--- setup.py
+++ setup.py
@@ -21,6 +21,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points={
+        'console_scripts': [
+            'olymptester = olymptester.olymptester:main',
+        ]
+    },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
